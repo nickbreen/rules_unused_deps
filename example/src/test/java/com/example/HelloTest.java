1fd37c6333bb1b4a3a5df3f95b982941ed6f7ba2
Nicholas Breen <nick@breen.kiwi>
2025-07-05 13:24:47 +1200

    add a test target

---
+++ example/src/test/java/com/example/HelloTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloTest
+{
+    @Test
+    public void shouldHello()
+    {
+        assertTrue("a fake test", true);
+    }
+}
